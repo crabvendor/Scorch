977a4b0dae4018c4e6bdaebbaf3bafbd18a1f65e
crabvendor <st.wyrobek@gmail.com>
2018-12-04 11:34:53 +0100

    Let TankController take tank as construcutor parameter

--- js/tank/TankController.js
+++ js/tank/TankController.js
@@ -1,15 +1,10 @@
 import { Tank } from "./Tank";
 
 export class TankController {
-    constructor(name, position) {
-        this.tank = this.createTank(name, position);
+    constructor(tank) {
+        this.tank = tank;
     }
-
-    createTank(name, position) {
-        let tank = new Tank(name, position);
-        return tank;
-    }
-
+    
     shoot() {
     }
 }
