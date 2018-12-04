ce1ffb6ccfbac281f14bb0261da512706cdd7940
crabvendor <st.wyrobek@gmail.com>
2018-12-04 11:27:44 +0100

    Implement TankView

---
+++ js/tank/TankView.js
@@ -0,0 +1,24 @@
+export class TankView {
+    constructor(tankController) {
+        this.tankController = tankController;
+        this.tankElement = this.createTankElement();
+    }
+
+    render() {
+        let name = this.tankController.tank.getName();
+        let positionLeft = this.tankController.tank.getPosition().getX();
+        return `
+        <div class="tank" id="${name}" style="left: ${positionLeft}px; top="calc(100% - 40px)"></div>
+        `
+    }
+
+    createTankElement() {
+        let elem = document.createElement('template');
+        elem.innerHTML = this.render().trim();
+        return elem.content.firstChild;
+    }
+
+    registerEventListeners() {
+        this.tankElement.addEventListener("click", this.tankController.shoot.bind(this.tankController));
+    }
+}
