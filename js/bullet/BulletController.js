50f55a2f484c6754f1d588e81c05a0e26ed1f72c
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-12-04 19:05:03 +0100

    Bullet now flies on desired trajectory

--- js/bullet/BulletController.js
+++ js/bullet/BulletController.js
@@ -8,16 +8,14 @@ export class BulletController{
         let angle = this.bullet.getAngle();
         let power = this.bullet.getPower();
         let trajectory = this.calulateTrajectory(power, angle, this.bullet.getPos());
-        let bulletElem = document.getElementById("bullet");
         let x = bulletElem.offsetLeft;
         bulletElem.style.display = "block";
-        let interval = setInterval((x) => {
-            x = bulletElem.offsetLeft;
-            for (let i = 0; i >= trajectory.length; i ++){
-                bulletElem.style.left = trajectory[i][0];
-                bulletElem.style.top = trajectory[i][1];
-            }
-            if (x > 1000) {
+        let counter = 0;
+        let interval = setInterval(() => {
+            bulletElem.style.left = trajectory[counter][0] + "px";
+            bulletElem.style.top = -trajectory[counter][1] + "px";
+            counter ++;
+            if (counter == trajectory.length) {
                 clearInterval(interval);
                 bulletElem.parentElement.removeChild(bulletElem);
             }
@@ -25,26 +23,24 @@ export class BulletController{
     }
 
     calulateTrajectory(power, angle, startPoint){
-        let angleRad = angle * 180/Math.PI
-        let gravityAcceleration = 9.8;    
+        let angleRad = angle * Math.PI/180
+        let gravityAcceleration = 9.8;
         
         let ySpeed = Math.sin(angleRad) * power;
         let xSpeed = Math.cos(angleRad) * power;
-        
         let xStart = startPoint.getX();
         let yStart = startPoint.getY();
-
-        let flyTime = Math.sqrt(2*(Math.pow(ySpeed, 2)/Math.pow(gravityAcceleration, 2) + yStart/gravityAcceleration));
-        
-        let TrajTable = [];
+        let stopTime = ySpeed/gravityAcceleration;
+        let flyTime = stopTime + Math.sqrt((ySpeed * stopTime - gravityAcceleration/2 * Math.pow(stopTime, 2) +40) * 2/gravityAcceleration);
+        let trajTable = new Array();
         
         for (let i = 0; i<flyTime; i += 0.2){
             let yValue = yStart + ySpeed * i - gravityAcceleration/2 * Math.pow(i, 2);
             let xValue = xSpeed * i + xStart;
-            TrajTable.push([xValue, yValue]);
+            trajTable.push([xValue, yValue]);
         }
 
 
-        return TrajTable;
+        return trajTable;
     }
 }
