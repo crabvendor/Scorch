66c63093ac1b2118e1d653cac04ab5f25c686429
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-12-04 19:03:40 +0100

    Added parameters for test shooting

--- js/tank/TankController.js
+++ js/tank/TankController.js
@@ -1,6 +1,7 @@
 import {Bullet} from "../bullet/Bullet.js";
 import {BulletView} from "../bullet/BulletView.js";
 import {BulletController} from "../bullet/BulletController.js";
+import { ShotParameter } from "./ShotParameter.js";
 
 export class TankController {
     constructor(tank) {
@@ -8,7 +9,8 @@ export class TankController {
     }
 
     shoot(e) {
-        let bullet = new Bullet(this.tank);
+        let shotParams = new ShotParameter(45, 50);
+        let bullet = new Bullet(this.tank, shotParams);
         let bulletController = new BulletController(bullet);
         let bulletView = new BulletView(bulletController);
         document.getElementById(`${this.tank.getName()}`).appendChild(bulletView.element);
