bd1ade927d19fe46d23d35c16b34e14285e4b175
crabvendor <st.wyrobek@gmail.com>
2018-12-04 11:27:15 +0100

    TankController constructor takes name and position parameters and creates Tank Object

--- js/tank/TankController.js
+++ js/tank/TankController.js
@@ -1,9 +1,10 @@
 import { Tank } from "./Tank";
 
 export class TankController {
-    constructor() {
+    constructor(name, position) {
+        this.tank = this.createTank(name, position);
     }
-    
+
     createTank(name, position) {
         let tank = new Tank(name, position);
         return tank;
