f2e211671e6d3a6fbe1a401db82287a6a646b52c
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-12-05 10:07:39 +0100

    Exported global constants to main js file

--- js/game.js
+++ js/game.js
@@ -5,6 +5,15 @@ import {Position} from "./position/Position.js";
 import {TankView} from "./tank/TankView.js";
 import {Tank} from "./tank/Tank.js";
 
+export const gravityAcceleration = 9.8;
+
+export const KeyCodes = {
+    LEFT:   37,
+    UP:     38,
+    RIGHT:  39,
+    DOWN:   40
+  };
+
 let tankPosition = new Position(0,0);
 let tank = new Tank("franek", tankPosition);
 let franekControler = new TankController(tank);
