310024c91283ad2c3a6fb05d0d590948c4315d79
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-12-05 10:09:33 +0100

    Started implementing method for managing shot parameters

--- js/tank/TankController.js
+++ js/tank/TankController.js
@@ -2,15 +2,27 @@ import {Bullet} from "../bullet/Bullet.js";
 import {BulletView} from "../bullet/BulletView.js";
 import {BulletController} from "../bullet/BulletController.js";
 import { ShotParameter } from "./ShotParameter.js";
+import { KeyCodes} from "../game.js";
 
 export class TankController {
     constructor(tank) {
         this.tank = tank;
+        this.shotParams = new ShotParameter(45,50);
     }
 
+    changeShotParams(e){
+        if (event.keyCode == KeyCodes.right){
+            this.shotParams.incrementPower();
+        }
+        if (event.keyCode == KeyCodes.UP){
+            this.shotParams.incrementAngle();
+        }
+
+        console.log(this.shotParams.getAngle() + "   " + this.shotParams.getPower);
+    } 
+
     shoot(e) {
-        let shotParams = new ShotParameter(45, 50);
-        let bullet = new Bullet(this.tank, shotParams);
+        let bullet = new Bullet(this.tank, this.shotParams);
         let bulletController = new BulletController(bullet);
         let bulletView = new BulletView(bulletController);
         document.getElementById(`${this.tank.getName()}`).appendChild(bulletView.element);
