6ad908d9215008378ae1daa3be5b322208254974
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-12-06 14:08:02 +0100

    Const values moved from bullet controller to container

--- js/Constants.js
+++ js/Constants.js
@@ -27,3 +27,10 @@ export const GroundElement = {
     WITDH:1,
     HEIGHT:1,
 };
+
+export const BulletControllerConsts = {
+    ANIMATION_SPEED_RATE: 0.15,
+    MOVE_SPEED_DELAY_MILISECS: 50,
+    TRAJECTORY_X_INDEX: 0,
+    TRAJECTORY_Y_INDEX: 1,
+}
